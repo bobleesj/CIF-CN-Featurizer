4bfe937404f9853b2f316178608290f04ade698e
bobleesj <bobleesj@gmail.com>
2023-10-09 13:55:29 -0400

    atom label ordering

--- preprocess/cif_parser.py
+++ preprocess/cif_parser.py
@@ -15,10 +15,16 @@ def get_atom_type(label):
     return None
 
 
+
 def extract_formula_and_atoms(block):
     """
     Extract the chemical formula and unique atoms from a CIF block.
     """
+
+    A_labels = ["Sc", "Y", "La", "Ce", "Pr", "Nd", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Th", "U"]
+    B_labels = ["Si", "Ga", "Ge", "In", "Sn", "Sb"]
+    M_labels = ["Fe", "Co", "Ni", "Ru", "Rh", "Pd", "Os", "Ir", "Pt"]
+    
     formula = None
     formula_string = None
 
@@ -29,18 +35,74 @@ def extract_formula_and_atoms(block):
     
     if formula:
         formula_string = formula[1]
-        formula_string = formula_string.replace("'", "") # Remove quotes
-        formula_string = re.sub('[~ ]', '', formula_string) # Remove dashes and spaces
+        formula_string = formula_string.replace("'", "")
+        formula_string = re.sub('[~ ]', '', formula_string)
 
     if not formula_string:
         return None, 0, None
 
-    pattern = re.compile(r"([A-Z][a-z]*)(\d*)") # Extract atom symbols and their counts
+    pattern = re.compile(r"([A-Z][a-z]*)(\d*)")
     matches = pattern.findall(formula_string)
     unique_atoms_tuple = [(atom, int(count) if count else 1) for atom, count in matches]
     num_of_unique_atoms = len({atom for atom, _ in unique_atoms_tuple})
+    sorted_unique_atoms_tuple = None
 
-    return unique_atoms_tuple, num_of_unique_atoms, formula_string
+    A_elements = []
+    B_elements = []
+    M_elements = []
+
+    for atom, count in unique_atoms_tuple:
+        if atom in A_labels:
+            A_elements.append((atom, count))
+        elif atom in B_labels:
+            B_elements.append((atom, count))
+        elif atom in M_labels:
+            M_elements.append((atom, count))
+
+
+    # Binary system
+    if num_of_unique_atoms == 2:
+        if len(A_elements) == 0 and len(B_elements) == 0:
+            sorted_unique_atoms_tuple = unique_atoms_tuple
+        elif len(A_elements) == 0 and len(B_elements) == 1:
+            A_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in B_labels]
+        elif len(B_elements) == 0 and len(A_elements) == 1:
+            B_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in A_labels]
+        
+        if len(A_elements) != 0 or len(B_elements) != 0:
+            sorted_unique_atoms_tuple = A_elements + B_elements
+
+    # Ternary system
+    if num_of_unique_atoms == 3:
+        if len(A_elements) == 0 and len(B_elements) == 0 and len(M_elements) == 0:
+            sorted_unique_atoms_tuple = unique_atoms_tuple
+        elif len(A_elements) == 1 and len(B_elements) == 1:
+            # One atom each in A and B: the remaining one should be M
+            M_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in A_labels and atom not in B_labels]
+        elif len(A_elements) == 0 and len(B_elements) == 1:
+            # One atom in B: the remaining ones should be A and M
+            M_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in B_labels]
+            A_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in B_labels and atom not in M_labels]
+        elif len(A_elements) == 1 and len(B_elements) == 0:
+            # One atom in A: the remaining ones should be B and M
+            M_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in A_labels]
+            B_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in A_labels and atom not in M_labels]
+        elif len(A_elements) == 1 and len(M_elements) == 1:
+            # One atom in A and one in M: the remaining one should be B
+            B_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in A_labels and atom not in M_labels]
+        elif len(B_elements) == 1 and len(M_elements) == 1:
+            # One atom in B and one in M: the remaining one should be A
+            A_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in B_labels and atom not in M_labels]
+        elif len(A_elements) == 0 and len(M_elements) == 1:
+            # One atom in M: the remaining ones should be A and B
+            A_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in B_labels and atom not in M_labels]
+            B_elements = [(atom, count) for atom, count in unique_atoms_tuple if atom not in A_labels and atom not in M_labels]
+        
+        if len(A_elements) != 0 or len(B_elements) != 0 or len(M_elements) != 0:
+            sorted_unique_atoms_tuple = A_elements + M_elements + B_elements
+
+    return sorted_unique_atoms_tuple, num_of_unique_atoms, formula_string
+
 
 
 def get_loop_tags():
